baceff6fcd61f505da6bc80fe51bb5306a6f338a
Dinesha G <120408570+Dine24@users.noreply.github.com>
2024-02-28 19:17:15 +0530

    Add files via upload

---
+++ Instructional Hazards.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+ int a = 5;
+ int b = 10;
+ int c;
+ int temp_a = a;
+ int temp_b = b;
+ c = temp_a + temp_b;
+ c = c * 2;
+ printf("Result: %d\n", c);
+ return 0;
+}
